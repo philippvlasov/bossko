76cad4e028aca356bde8113681af44447d9bef0b
philipp_vlasov <vlasov.philipp@gmail.com>
2020-03-28 09:51:13 +0200

    Removed all files (WIP)

--- app/js/script.js
+++
@@ -1,5 +0,0 @@
-// Instantiating the global app object
-var app = {};
-var number = 1;
-
-console.log($('h1'));
--- order.php
+++
@@ -1,9 +0,0 @@
-<?php
-$area = $_POST['area'];
-$type = $_POST['type'];
-/*$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail('aksenov.andrew@gmail.com', 'test', '$message', $headers); //отправляет получателю на емайл значения переменных
-*/
-echo $area . '<br>' . $type;
-?>
