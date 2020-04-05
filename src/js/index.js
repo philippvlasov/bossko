941d188294ca08f58fbe2e5ab158524e0a4fa0ba
philipp_vlasov <vlasov.philipp@gmail.com>
2020-04-05 13:29:19 +0300

    Fixed merge errors

--- src/js/index.js
+++ src/js/index.js
@@ -153,7 +153,6 @@ $(document).ready(function () {
             event.preventDefault();
             event.stopPropagation();
             const targetSection = this.$container.attr('href');
-            console.log(this.$container.attr('href'));
             $('html, body').animate({
                 scrollTop: $(`.${targetSection}`).offset().top
             }, 500);
@@ -171,3 +170,38 @@ $(document).ready(function () {
         instance.init();
     });
 }(jQuery));
+
+$('#callback').on('submit', function () {
+    event.preventDefault();
+    const form = $('#callback').find('form')[0];
+    const data = new FormData(form);
+
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: "callback.php",
+        data: data,
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 800000,
+        success: function (data) {
+            console.log("SUCCESS : ", data);
+            const $modal = $('#success');
+            const $callbackModal = $('#callback');
+            const result = $($modal[0]).find('.result');
+            result.text(data);
+            $callbackModal.modal('hide');
+            $modal.modal('show');
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+            const $modal = $('#fail');
+            const result = $($modal[0]).find('.result');
+            result.text('Произошла ошибка отправки формы. Свяжитесь с нами по телефону +7-9999-777-550');
+            $modal.modal('show');
+
+        }
+    });
+    return;
+})
